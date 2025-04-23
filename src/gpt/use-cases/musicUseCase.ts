9a2472161ebefd3155ff0e4b1138cc7c805f876d
Juan Andres Alvarez <juanandres1029@gmail.com>
2025-04-23 10:28:38 -0500

    Add ai response

--- src/gpt/use-cases/musicUseCase.ts
+++ src/gpt/use-cases/musicUseCase.ts
@@ -7,25 +7,25 @@ interface Options {
 
 export const musicUseCase = async(openai: OpenAI, options: Options) => {
     const { prompt } = options;
-    const completion = openai.chat.completions.create({
+    const completion = openai.responses.create({
         model: "gpt-4o-mini",
-        temperature: 0.3,
-        max_tokens: 600,
+        temperature: 0.7,
+        
         store: true,
-        messages: [
+       
+        input: [
           {"role": "user", "content": `Eres un profesor experto de música, tu misión será sugerir al estudiante 
             la mejor manera de abordar las tareas que el estudiente te entregue. Deberás entregarle el cómo  abordar cada tarea 
              de acuerdo a la información proporcionada o lista de tareas a continuacion: 
             ${prompt}. Las respuestas deben ser muy técnicas y estar basadas en el contexto musical, si las tareas no tienen que ver con música, debes informarle al usuario.
             `},
         ],
-
+       
       });
 
-      
-    console.log(completion)
-     
+      //const response = JSON.parse((await completion).choices[0].message.content)
     
-    return completion;
+    
+    return (await completion).output[0];
     
 }
